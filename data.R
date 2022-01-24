d8dd76058456303eb8597eb7e0a3e272a45da501
jjmoncus <jjmoncus706@gmail.com>
2022-01-23 20:57:39 -0500

    Starting gathering data

---
+++ data.R
@@ -0,0 +1,18 @@
+
+library(spotifyr)
+
+
+
+id <- "f3c7070d893f4c3480176c8bdfc5e6d9"
+secret <- "0b73daa97da84b2ab7ac0eba8b174f85"
+
+access_token <- get_spotify_access_token(client_id = id,
+                                         client_secret = secret)
+auth_code <- 
+
+
+get_my_profile(access_token)
+
+
+
+
